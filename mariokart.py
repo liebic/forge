735b7f17e1f2311dbe2ba702f83720c3c643b5ea
Zachary Chenoweth <zchenowe@terpmail.umd.edu>
2023-10-31 18:55:33 -0400

    Added mariokart.py and created a skeleton for mystery_box function

---
+++ mariokart.py
@@ -0,0 +1,9 @@
+"""Simulates a game of mario kart through randomized events and text-based prompts.
+"""
+
+def mystery_box(player_rank):
+"""Determines the type of item that a player will recieve from a mystery box
+
+Args:
+    player_rank(int): The pacement of the player in the race.
+"""
