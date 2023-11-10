01812c1e9a52e63bae75c421889c6e7e6a56ef09
liebic <132029087+liebic@users.noreply.github.com>
2023-11-10 14:07:00 -0500

    My part for proposal

--- mariokart.py
+++ mariokart.py
@@ -2,7 +2,8 @@
 """
 class MarioKart:
     """
-    A class representing mario kart, that includes a list of players and available characters.
+    A class representing mario kart, that includes a list of players and 
+    available characters.
 
     Attributes: 
     players(list): A list of players in the game
@@ -11,6 +12,58 @@ class MarioKart:
     Returns: prints available players
     """
     def __init__(self):
+      """Initialize a MarioKart instance.
+      """
+      self.players = [] #initializing an empty list of players names
+      self.available_characters = ["Mario", "Luigi", "Princess Peach", "Bowser"]
+      
+    def set_players(self, players_names):
+      """Set the list of players who are participating in the MarioKart game.
+
+      Args:
+          players_names (list): A list containing the names of the players
+
+      Raises:
+          TypeError: If the player_names is not on the list
+          ValueError: If the number of players is more than 4
+      """
+      if not isinstance(players_names, list): #making sure players name is on the list
+        raise TypeError("The players_list must be on the list! Try again.")
+      if len(players_names) > 4: #checking to see if the number of players exceed 4
+        raise ValueError("Cannot have more than four players, sorry! Try \
+          playing again next round.")
+      self.players = players_names
+      
+    def get_available_characters(self):
+      """Return the list of available characters
+
+      Returns:
+          list: A list containing the names of the available characters.
+      """
+      return self.available_characters
+    
+    def choosing_character(self, player, character):
+      """A player is choosing what character they'll play as.
+
+      Args:
+          player (str): The name of the player choosing the character.
+          character (str): The name of the character being chosen.
+          
+      Side effects:
+        prints results of character selection
+      """
+      # printing available characters before the player chooses
+      print("Available characters are: ")
+      for char in self.available_characters:
+        print(char)
+      
+      if character in self.available_characters:
+        self.available_characters.remove(character)
+        self.players.append((player, character)) # adding the player and their
+          #chosen character to the list of characters
+        print(f"{player} has chosen {character}!")
+      else:
+        print(f"Sorry, {character} has already been chosen. Please choose another.")
     
     
     def mystery_box(player_rank):
@@ -58,6 +111,12 @@ class Player:
 
 
 
+def player_position():
+    """ Determine what play the player is after using special item.
+    
+    Returns: Player Position and whether or not they are in top 3.
+    """
+
 def burn_out_probability(player):
   """Calculates the probability of a player burning out at the start of a race.
 
@@ -68,7 +127,7 @@ def burn_out_probability(player):
     A float representing the probability of the player burning out.
   """
   
-  def prob_hit_player(player_rank, speed):
+def prob_hit_player(player_rank, speed):
     ''' Determines the probability a player is hit by another player. 
     
       Args: 
@@ -79,7 +138,7 @@ def burn_out_probability(player):
     '''
     
     
-  def prob_hit_player(player_rank, speed):
+def prob_hit_player(player_rank, speed):
       ''' Determines the probability a player is hit by another player. 
       
       Args: 
