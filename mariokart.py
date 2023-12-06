aed6dab078527198d3a8e68b0416dede095fea03
yann2142 <143436345+yann2142@users.noreply.github.com>
2023-12-05 19:50:05 -0500

    Update mariokart.py

--- mariokart.py
+++ mariokart.py
@@ -1,168 +1,202 @@
-"""Simulates a game of mario kart through randomized events and text-based prompts.
-"""
 import random
 import re
-class MarioKart:
-    """
-    A class representing mario kart, that includes a list of players and 
-    available characters.
-
-    Attributes: 
-    players(list): A list of players in the game
-    available_characters(list): A list of characters a player can choose from
-    
-    Returns: prints available players
-    """
-    def __init__(self, players_info=[]): 
-      """Initialize a MarioKart instance.
-      """
-      self.players = [] #initializing an empty list of players names
-      self.available_characters = ["Mario", "Luigi", "Princess Peach", "Bowser"]
-      
-      for player, character in players_info:
-        self.choosing_character(player, character)
-      
-    def set_players(self, players_names):
-      """Set the list of players who are participating in the MarioKart game.
-
-      Args:
-          players_names (list): A list containing the names of the players
-
-      Raises:
-          TypeError: If the player_names is not on the list
-          ValueError: If the number of players is more than 4
-      """
-      if not isinstance(players_names, list): #making sure players name is on the list
-        raise TypeError("The players_list must be on the list! Try again.")
-      if len(players_names) > 4: #checking to see if the number of players exceed 4
-        raise ValueError("Cannot have more than four players, sorry! Try \
-          playing again next round.")
-      self.players = players_names
-      
-    def get_available_characters(self):
-      """Return the list of available characters
-
-      Returns:
-          list: A list containing the names of the available characters.
-      """
-      return self.available_characters
-    
-    def choosing_character(self, player, character):
-      """A player is choosing what character they'll play as.
-
-      Args:
-          player (str): The name of the player choosing the character.
-          character (str): The name of the character being chosen.
-          
-      Side effects:
-        prints results of character selection
-      """
-      # printing available characters before the player chooses
-      print("Available characters are: ")
-      for char in self.available_characters:
-        print(char)
-        
-      player = input("")
-      
-      if character in self.available_characters:
-        self.available_characters.remove(character)
-        self.players.append((player, character)) # adding the player and their
-          #chosen character to the list of characters
-        print(f"{player} has chosen {character}!")
-      else:
-        print(f"Sorry, {character} has already been chosen. Please choose another.")
-    
-    
-    def mystery_box(player_rank):
-      """Determines the type of item that a player will recieve from a mystery box
-      
-      Args:
-        player_rank(int): The placement of the player in the race.
-    """
-      with open("items.txt", 'r') as file:
-        for item in file:
-          if re.match(r'[A-Za-z]+(-?)[A-Za-z]+', item):
-            each_item = item.split(" ")
-        if player_rank == 1:
-            rand_item = random.choice(each_item[0:4])
-        elif player_rank == 2:
-            rand_item = random.choice(each_item[0:7])
-        elif player_rank == 3:
-            rand_item = random.choice(each_item[0:11])
-        elif player_rank == 4:
-          rand_item = random.choice(each_item[0:14])
-        return print(f'Player in {player_rank} place received: {rand_item}')
-      def prob_hit_player(num_players, player_rank, speed):
-        ''' Determines the probability a player is hit by another player. 
-        Args: 
-        num_players(int): number of players playing 
-        player_rank(int): The placement of the player in the race.
-        speed (int): speed of player. This will be a number between 1 and 100.  
-        Returns: an f string that contains the prob of player hitting another player'''
-        if speed > 100:
-            return "Choose a speed between 0 and 100 mph."
-        #create rank_factor. This depicts that the probability of hitting another player will likey change as player rank changes.
-        rank_factor= player_rank/num_players
-        #create speed_factor. This depicts that the probability of hitting another player will change  as a player's speed changes 
-        speed_factor = speed/100
-        probability_hit_player=(rank_factor+speed_factor)/2
-        #ensure probability is between 0 and 1 
-        probability_hit_player=max(0, min(1,probability_hit_player))
-        probability_hit_player=round(probability_hit_player,2)
-        return f"The probability of hitting another player is {probability_hit_player}."
 
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.position = 0
-
-    def adjust_position(self, position_change):
-        self.position += position_change
-
-    def player_position(player, is_hit_by_special_item, is_using_special_item):
+    def __init__(self, name, position):
         """
-        Determine the player's position and whether they are in the top 3 after using a special item or being hit by one.
+        Initialize a Player instance.
 
-        Args:
-        player (Player): The Player object for the player.
-        is_hit_by_special_item (bool): True if the player is hit by a special item, False if not.
-        is_using_special_item (bool): True if the player is using a special item, False if not.
+        Parameters:
+        - name (str): The name of the player.
+        - position (int): The starting position of the player.
+        """
+        self.name = name
+        self.character = None
+        self.position = position
+        self.speed = random.randint(1, 10)
+        self.wins = 0
+
+    def adjust_position(self, finishing_order, item_effect):
+        """
+        Adjust the player's position based on finishing order and item effect.
+
+        Parameters:
+        - finishing_order (int): The finishing order of the player.
+        - item_effect (str): The effect of the item received.
+        """
+        position_change = 0
+
+        if item_effect == "Star":
+            position_change = 2
+        elif item_effect == "Bullet-Bill":
+            position_change = 3
+
+        self.position = max(1, finishing_order + 1 + position_change)
+
+    def player_position(self, finishing_order, item_effect, is_hit_by_special_item, is_using_special_item):
+        """
+        Update player position based on special items.
+
+        Parameters:
+        - finishing_order (int): The finishing order of the player.
+        - item_effect (str): The effect of the item received.
+        - is_hit_by_special_item (bool): Whether the player is hit by a special item.
+        - is_using_special_item (bool): Whether the player is using a special item.
+        """
+        if is_hit_by_special_item or is_using_special_item:
+            self.adjust_position(finishing_order, item_effect)
+
+    def burn_out_probability(self, item_effect):
+        """
+        Calculate burnout probability based on the item effect.
+
+        Parameters:
+        - item_effect (str): The effect of the item received.
 
         Returns:
-        player position indicating whether they are in the top 3 or not.
+        - float: The calculated burnout probability.
         """
-        if is_hit_by_special_item:
-            player.adjust_position(-2)
-        if is_using_special_item:
-            player.adjust_position(3)
+        base_probability = random.random()
+        speed_factor = self.speed / 10.0
 
-        players = sorted(players, key=lambda p: p.position, reverse=True)
-        player_index = players.index(player)
-        player_position = player_index + 1 
+        if item_effect in ["Mushroom", "Triple-Mushroom"]:
+            speed_factor *= 1.2
+        elif item_effect == "Golden-Mushroom":
+            speed_factor *= 1.5
 
-        in_top_3 = player_index < 3
+        burn_out_probability = base_probability * speed_factor
+        burn_out_probability = max(0.0, min(1.0, burn_out_probability))
+        return burn_out_probability
 
-        return print(f"{player.name}: Position {player_position}, In Top 3: {in_top_3}")
+    def __str__(self):
+        return f"{self.name} ({self.character}): Position {self.position} - Wins {self.wins}"
 
-def burn_out_probability(player):
-  """Calculates the probability of a player burning out at the start of a race.
+    def mystery_box(self, player_rank):
+        """
+        Simulate the player receiving an item from the mystery box.
 
-  Args:
-    player: A Player object.
+        Parameters:
+        - player_rank (int): The rank of the player.
 
-  Returns:
-    A float representing the probability of the player burning out.
-  """
-  base_probability = random.random() # base probability selected randomly
-  speed_factor = player.speed / 10.0  # speed is on a scale of 0 to 10
+        Returns:
+        - str: The item received from the mystery box.
+        """
+        with open("items.txt", 'r') as file:
+            items = [item.strip() for item in file.read().split(",")]
 
-  # calculates burn-out probability by base and player speed
-  burn_out_probability = base_probability * speed_factor
+            if player_rank == 1:
+                max_items = min(4, len(items))
+            elif player_rank == 2:
+                max_items = min(7, len(items))
+            elif player_rank == 3:
+                max_items = min(11, len(items))
+            elif player_rank == 4:
+                max_items = min(14, len(items))
 
-  # keeps probability as a float between 0 and 1
-  burn_out_probability = max(0.0, min(1.0, burn_out_probability))
+            player_item = random.choice(items[:max_items])
 
-  return burn_out_probability
+            print(f'{self.name} in {self.position} place received: {player_item}')
+            return player_item
+
+
+class MarioKart:
+    def __init__(self):
+        """
+        Initialize a MarioKart instance.
+        """
+        self.players = []
+        self.available_characters = ["Mario", "Luigi", "Princess Peach", "Bowser", "Daisy", "Shy Guy", "Toad", "Wario", "Link", "Donkey Kong", "Waluigi", "King Boo"]
+
+    def set_players(self, players_names):
+        """
+        Set the players for the game.
+
+        Parameters:
+        - players_names (list): A list of player names.
+        """
+        if not isinstance(players_names, list):
+            raise TypeError("The players_names parameter must be a list! Try again.")
+        if len(players_names) > 4:
+            raise ValueError("Cannot have more than four players. Try playing again next round.")
+        positions = random.sample(range(1, 5), len(players_names))
+        self.players = [Player(name, position) for name, position in zip(players_names, positions)]
+
+    def choosing_character(self, player):
+        """
+        Allow a player to choose a character for the game.
+
+        Parameters:
+        - player (Player): The player choosing a character.
+        """
+        print("\nAvailable characters are: ")
+        for char in self.available_characters:
+            print(char)
+
+        while True:
+            character = input(f"{player.name}, choose a character: ").capitalize()
+
+            if character.lower() in [char.lower() for char in self.available_characters]:
+                self.available_characters.remove([char for char in self.available_characters if char.lower() == character.lower()][0])
+                print(f"{player.name} has chosen {character}!")
+                player.character = character
+                break
+            else:
+                print(f"Sorry, {character} is not available or has already been chosen. Please choose another.")
+
+    def run_round(self):
+        """
+        Run a round of the Mario Kart game.
+        """
+        print("\nCurrent Player Positions:")
+        for player in self.players:
+            item_effect = player.mystery_box(player.position)
+            burnout_probability = player.burn_out_probability(item_effect)
+
+            print(f'{player} - Burnout Probability: {burnout_probability:.2f}')
+
+            player.player_position(
+                self.players.index(player),
+                item_effect,
+                random.choice([True, False]),
+                random.choice([True, False])
+            )
+
+        self.players.sort(key=lambda player: player.position)
+
+        print("\nRound Winner:")
+        round_winner = self.players[0]
+        print(round_winner)
+        round_winner.wins += 1
+
+    def display_overall_winner(self):
+        """
+        Display the overall winner of the Mario Kart game.
+        """
+        self.players.sort(key=lambda player: player.wins, reverse=True)
+
+        tied_players = [player for player in self.players if player.wins == self.players[0].wins]
+
+        if len(tied_players) == 1:
+            print("\nTiebreaker Round for Tied Players:")
+            for player in tied_players:
+                self.choosing_character(player)
+            self.run_round()
+
+            self.players.sort(key=lambda player: player.wins, reverse=True)
+
+        print("\nOverall Winner:")
+        print(f"{self.players[0].name} ({self.players[0].character}) with {self.players[0].wins} wins!")
+
 
 if __name__ == "__main__":
-  mario_kart_game = MarioKart()
+    mario_kart_game = MarioKart()
+
+    for _ in range(3):  # Run 3 rounds
+        mario_kart_game.set_players(["Player1", "Player2", "Player3", "Player4"])
+
+        for player in mario_kart_game.players:
+            mario_kart_game.choosing_character(player)
+
+        mario_kart_game.run_round()
+
+    mario_kart_game.display_overall_winner()
