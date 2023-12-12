619f09e5a7c71969c7de871ba8de446a8233d082
liebic <132029087+liebic@users.noreply.github.com>
2023-12-11 21:20:12 -0500

    fixing code

--- mariokart.py
+++ mariokart.py
@@ -1,5 +1,7 @@
 import random
 import re
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 class Player:
     def __init__(self, name, position):
@@ -55,9 +57,6 @@ class Player:
 
         Returns:
         - float: The calculated burnout probability.
-        
-        Side Effects:
-        - Returns burn out probability 
         """
         base_probability = random.random()
         speed_factor = self.speed / 10.0
@@ -70,22 +69,60 @@ class Player:
         burn_out_probability = base_probability * speed_factor
         burn_out_probability = max(0.0, min(1.0, burn_out_probability))
         return burn_out_probability
-    
+
+    def __str__(self):
+        return f"{self.name} ({self.character}): Position {self.position} - Wins {self.wins}"
+
+    def mystery_box(self, player_rank):
+        """
+        Simulate the player receiving an item from the mystery box.
+
+        Parameters:
+        - player_rank (int): The rank of the player.
+
+        Returns:
+        - str: The item received from the mystery box.
+        """
+        with open("items.txt", 'r') as file:
+            items = [item.strip() for item in file.read().split(",")]
+            max_items = 0
+
+            if player_rank == 1:
+                max_items = min(4, len(items))
+            elif player_rank == 2:
+                max_items = min(7, len(items))
+            elif player_rank == 3:
+                max_items = min(11, len(items))
+            elif player_rank == 4:
+                max_items = min(14, len(items))
+
+            player_item = random.choice(items[:max_items])
+
+            print(f'{self.name} in {self.position} place received: {player_item}')
+            return player_item
+
+
 class MarioKart:
     def __init__(self):
         """
         Initialize a MarioKart instance.
         """
         self.players = []
-        self.available_characters = ["Mario", "Luigi", "Princess Peach", "Bowser", "Daisy", "Shy Guy", "Toad", "Wario", "Link", "Donkey Kong", "Waluigi", "King Boo", "Yoshi", "Birdo", "Lakitu"]
-        self.available_vehicles= ["Bike","Sprinter","Wild Wiggler","Kart","Mach Bike","Biddybuggy","Flame Runner","Spear"]
+        self.available_characters = ["Mario", "Luigi", "Princess Peach", "Bowser","Daisy", "Shy Guy", "Toad", "Wario", "Link", "Donkey Kong", "Waluigi", "King Boo", "Yoshi", "Birdo", "Lakitu"]
+        self.available_vehicles= ["Bike","Sprinter","Wild Wiggler","Kart","Mach Bike","Biddybuggy","Flame Runner","Spear", "Hovercraft", "Turbo Bike", "Rocket Car", "Shoe Car", "Box Car" , "Ghost Car"]
 
     def set_players(self, players_names):
         """
         Set the players for the game.
 
         Parameters:
-        - players_names (list): A list of player names.
+            players_names (list): A list of player names.
+        
+        Side Effects:
+            Prints the available character(s)
+            Takes user input to choose a character, updates player information
+            Prints message regarding successful character selection or prompts
+                player to choose another if selected character is already chosen
         """
         if not isinstance(players_names, list):
             raise TypeError("The players_names parameter must be a list! Try again.")
@@ -99,21 +136,32 @@ class MarioKart:
         Allow a player to choose a character for the game.
 
         Parameters:
-        - player (Player): The player choosing a character.
+            player (str): The player choosing a character.
+            
+        Raises:
+            TypeError: If the players_names parameter is not a list
+            ValueError: If there are more than 4 players
+        
+        Side Effects:
+            Prints a message indicating successful player setup
         """
         print("\nAvailable characters are: ")
         for char in self.available_characters:
             print(char)
+            
+        while True:
+            character = input(f"{player.name}, choose a character: ").capitalize()
+
+            if character.lower() in [char.lower() for char in self.available_characters]:
+                self.available_characters.remove([char for char in self.available_characters if char.lower() == character.lower()][0])
+                print(f"{player.name} has chosen {character}!")
+                player.character = character
+                break
+            else:
+                print(f"Sorry, {character} is not available or has already been chosen. Please choose another.")
+
+        
     def choosing_vehicle(self, player):
-        """
-        Allows a player to choose a vechile that they will play the game with. 
-        Parameters: 
-        player(Player): The player choosing a character
-        Side effects:
-        -prints the available vechiles
-        -updates the player.vechile and player.character based on whatever the player chooses. 
-        -uses print statements to inform the player of various information
-        """
         print("Available vehicles are: ", ", ".join(self.available_vehicles))
         while True:
             chosen_vehicle = input(f"{player.name}, choose a vehicle: ").capitalize()
@@ -126,25 +174,9 @@ class MarioKart:
             else:
                 print(f"Sorry, {chosen_vehicle} has already been chosen for this round. Please choose another.")
 
-        while True:
-            character = input(f"{player.name}, choose a character: ").capitalize()
-
-            if character.lower() in [char.lower() for char in self.available_characters]:
-                self.available_characters.remove([char for char in self.available_characters if char.lower() == character.lower()][0])
-                print(f"{player.name} has chosen {character}!")
-                player.character = character
-                break
-            else:
-                print(f"Sorry, {character} is not available or has already been chosen. Please choose another.")
-
     def run_round(self):
         """
-        Allows a round of Mario Kart to be played. 
-        Side effects:
-        - Burnout probability is used to update player positions
-        - player list is sorted based on updated positions 
-        -The player with the highest position at the end of the round is considered the winner for that particular round. 
-        -information about each plater's position and burnout probability is printed
+        Run a round of the Mario Kart game.
         """
         print("\nCurrent Player Positions:")
         for player in self.players:
@@ -168,19 +200,6 @@ class MarioKart:
     def display_overall_winner(self):
         """
         Display the overall winner of the Mario Kart game.
-
-        Parameters:
-        - None
-
-        Returns:
-        - None
-
-        Side Effects:
-        - Modifies the order of players in the self.players list.
-        - Prints information about tiebreaker rounds if there is a tie.
-        - Calls the choosing_character method and the run_round method if there is a tie.
-        - Calls the plot_final_ranking method to display the final ranking.
-        - Prints information about the overall winner.
         """
         self.players.sort(key=lambda player: player.wins, reverse=True)
 
@@ -193,6 +212,8 @@ class MarioKart:
             self.run_round()
 
             self.players.sort(key=lambda player: player.wins, reverse=True)
+
+        self.plot_final_ranking(self.players)
 
         print("\nOverall Winner:")
         print(f"{self.players[0].name} ({self.players[0].character})!")
